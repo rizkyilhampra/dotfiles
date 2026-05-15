d041a7de3f9bf3fe6fff6b2e8891f640b0fbd7da
rizkyilhampra <rizkyilhamp16@gmail.com>
2026-05-15 22:05:17 +0800

    chore(wallpaper): replace swww with awww

--- .config/hypr/hyprland/scripts/wallpaper.sh
+++ .config/hypr/hyprland/scripts/wallpaper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-swww-daemon --format xrgb
+awww-daemon --format xrgb
 
 PIC=$HOME/Wallpapers/1.jpeg
 
-swww img "${PIC}" --transition-type wipe --transition-fps 60 --transition-duration 2 --transition-angle 30 --transition-step 90
+awww img "${PIC}" --transition-type wipe --transition-fps 60 --transition-duration 2 --transition-angle 30 --transition-step 90
 
 if [ ! -e "${HOME}/.cache/wal/colors" ]; then
     wal -i "${PIC}"
